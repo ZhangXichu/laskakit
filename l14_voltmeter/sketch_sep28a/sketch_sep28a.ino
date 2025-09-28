c9a109619f8017e76a818803c2321b492c7636e2
ZhangXichu <xichu.zhang@yahoo.com>
2025-09-28 15:52:59 +0200

    voltmeter

---
+++ l14_voltmeter/sketch_sep28a/sketch_sep28a.ino
@@ -0,0 +1,31 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+int volt = A0;
+
+void setup() {
+  Serial.begin(9600);
+  unsigned long start = millis();
+  while (!Serial && millis() - start < 3000) { } 
+
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(5,0);
+  lcd.print("Voltage:");
+}
+
+void loop() {
+  // 10-bit ADC: 0..1023. Use 1023.0 for correct scaling
+  float val_raw = analogRead(volt);
+  float val = val_raw / 1023 * 5.0;
+
+  Serial.print(val);
+  Serial.println("V");
+
+  lcd.setCursor(6,1);
+  lcd.print(val);
+  lcd.print("V");
+  delay(300);
+}
